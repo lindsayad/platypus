dd8d518eb3e9306312326a302400b6b8d5128fff
Alex Lindsay <alexander.lindsay@inl.gov>
2024-11-26 13:57:22 -0800

    Remove rt library from linking

--- platypus.mk
+++ platypus.mk
@@ -2,7 +2,7 @@ MFEM_DIR			?=$(APPLICATION_DIR)/../mfem/build
 MFEM_INC_DIR                    ?=$(MFEM_DIR)/include
 
 MFEM_INCLUDES 		:= -I$(MFEM_DIR)/include
-MFEM_LIBS 			:= -Wl,-rpath,$(MFEM_DIR)/lib -L$(MFEM_DIR)/lib -lmfem -lrt -lmfem-common
+MFEM_LIBS 			:= -Wl,-rpath,$(MFEM_DIR)/lib -L$(MFEM_DIR)/lib -lmfem -lmfem-common
 
 ADDITIONAL_INCLUDES += $(MFEM_INCLUDES)
 ADDITIONAL_LIBS 	+= $(MFEM_LIBS) ${MFEM_EXT_LIBS}
